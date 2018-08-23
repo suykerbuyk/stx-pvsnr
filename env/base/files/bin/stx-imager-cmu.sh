294f909ed5f57f48a52ebe287fd8502200f3016f
John Suykerbuyk <john.suykerbuyk@seagate.com>
2018-08-23 10:26:00 -0600

    Add a bit of verbosity to the stx-imager-cmu script

--- env/base/files/bin/stx-imager-cmu.sh
+++ env/base/files/bin/stx-imager-cmu.sh
@@ -13,7 +13,7 @@ function showdone() {
 	exit 0
 }
 function wipedisk() {
-# Destroy Raid file systems (should destroy LVM groups first!)
+echo "wipedisk: Destroy Raid file systems (should destroy LVM groups first!)"
 if [ -d /dev/md/ ]; then
 	for dev in $(find /dev/md*p? -type b); do
 		wipefs -fa ${dev}
@@ -21,17 +21,20 @@ if [ -d /dev/md/ ]; then
 fi
 RAID_MEMBERS=''
 for disk in $(blkid | grep 'linux_raid_member' | awk -F ':' '{print $1}'); do
-	echo adding: $disk
+	echo "wipedisk: Adding $disk to list of known raid members"
 	RAID_MEMBERS="${RAID_MEMBERS} ${disk}"
 done
 for raid in $(find /dev/ -type b -name "md*" | grep -v 'p'); do
+	echo "wipedisk: Stopping raid device: ${raid}"
 	mdadm --stop ${raid}
 done
 for disk in ${RAID_MEMBERS}; do
+	echo "wipedisk: Zeroing Super Block of ${disk}"
 	mdadm --zero-superblock "${disk}"
 done
 
 for disk in $(blkid | grep sd | grep 'LABEL'  | awk -F ':' '{print $1}' ); do
+	echo "wipedisk: Clearing file system of ${disk}"
 	wipefs -fa ${disk}
 done
 partprobe
@@ -68,86 +71,88 @@ DONE_MARKER=/root/provisioning.done
 # echo MNT1:   $MNT1
 # echo MNT2:   $MNT2
 
-# Clean up all the stuff that is there
+echo "Cleaning up all the stuff that is there"
 wipedisk
 wipefs -fa "${DSK}"
 
 # Capture the complete command line to echo it later
 CMD="curl -s ${URL} | tar -xJOf - | dd of=${DSK} bs=1M conv=fdatasync"
-echo CMD: $CMD
 
-# Create our mountpoints
+echo "Creating necessary mountpoints:"
+echo "MNT1: ${MNT1}"
+echo "MNT2: ${MNT2}"
 [ -d ${MNT1} ] || mkdir -p ${MNT1} 
 [ -d ${MNT2} ] || mkdir -p ${MNT2} 
 
-# Make sure that things aren't already mounted.
+echo "Making sure that things aren't already mounted."
 mount -l | grep "${MNT2}" >/dev/null && umount "${MNT2}"
 mount -l | grep "${MNT1}/var" >/dev/null && umount "${MNT1}/var"
 mount -l | grep "${MNT1}" >/dev/null && umount "${MNT1}"
 
-# Execute the imaging command
+echo "Executing the imaging command"
+echo "CMD: $CMD"
 eval "$CMD" || exit 1
 
-# Update kernel's partition tables
+echo "Update kernel's partition tables"
 partprobe ${DSK}
 
-# Remove the original single partition.
+echo "Remove the original single partition."
 parted "${DSK}" rm 1 >/dev/null
 
 # Recreate the first partion, but larger.
 parted "${DSK}" mkpart primary ext4 1049kb 12900MB >/dev/null
 
-# Create the second (/var) partition
+echo "Create the second (/var) partition"
 parted "${DSK}" mkpart primary ext4 12901MB '100%' >/dev/null
 
-# Set the "clean" flag on the resized root partition.
+echo "Setting the 'clean' flag on the resized root partition."
 e2fsck -fp "${DSK}1" >/dev/null
 
-# Stretch the root partion to fill the space available
+echo "Stretch the root partion to fill the space available"
 resize2fs "${DSK}1"
 
-# Create the file system for /var
+echo "Create the file system for /var"
 mkfs.ext4 -q -L VAR /dev/sda2 >/dev/null
 
-# Update the kernel's partition tables.
+echo "Update the kernel's partition tables."
 partprobe ${DSK} >/dev/null
 
-# Mount the new root partition
+echo "Mount the new root partition"
 mount ${DSK}1 ${MNT1}
 
-# Mount what will be the /var partition
+echo "Mount what will be the /var partition"
 mount ${DSK}2 ${MNT2}
 
-# Copy contents of original /var to new dedicated partion
+echo "Copy contents of original /var to new dedicated partion"
 rsync -ar ${MNT1}/var/ ${MNT2}/ --remove-source-files
 
-# Clean up old var directory.
+echo "Clean up old var directory"
 rm -rf ${MNT1}/var/*
 
-# Capture the UUID's of the new partitions
+echo "Capturing the UUID's of the new partitions"
 UUID_P1=$(blkid ${DSK}1 | awk -F '"' '{print $4}')
 UUID_P2=$(blkid ${DSK}2 | awk -F '"' '{print $4}')
-#echo UUID_P1 = $UUID_P1
-#echo UUID_P2 = $UUID_P2
+echo "UUID_P1: $UUID_P1"
+echo "UUID_P2: $UUID_P2"
 
-# Build a new /etc/fstab for the new partitions
+echo "Adding new /etc/fstab entries for the new partitions"
 echo "UUID=${UUID_P1} / ext4 defaults 1 1"     >${MNT1}/etc/fstab
 echo "UUID=${UUID_P2} /var ext4 defaults 1 1" >>${MNT1}/etc/fstab
 
-# Remove the new /var partion mount
+echo "Removing the old (temporary) mount for the new /var"
 umount "${MNT2}"
 rmdir "${MNT2}" 
 
-# Mount the new /var where it should be on the new root partion
+echo "Mounting the new /var where it should be on the new root partion"
 mount /dev/disk/by-uuid/${UUID_P2} ${MNT1}/var
 
-# Set the name of the salt master
+echo "Setting the name of the salt master"
 sed -i "s/^#* *master:.*/master: stx-prvsnr/g" ${MNT1}/etc/salt/minion
 
-# Clean up old repos
+echo "Cleaning up old repos"
 rm -rf ${MNT1}/etc/yum.repos.d/*
 
-
+echo "Adding stx-prvsnr repos"
 cat >${MNT1}/etc/yum.repos.d/base.repo << EOF
 [base]
 name=base
@@ -175,7 +180,7 @@ EOF
 rpm --root ${MNT1}/ --import  http://stx-prvsnr/vendor/centos/7.5.1804/RPM-GPG-KEY-CentOS-7
 yum clean all --installroot ${MNT1}/
 yum update --installroot ${MNT1}/
-yum install -y salt-minion  --installroot ${MNT1}/
+yum install -y salt-minion salt-master salt --installroot ${MNT1}/
 
 # Copy preauthenticated salt minion keys
 [ -d ${MNT1}/etc/salt/pki/minion/ ] || mkdir -p ${MNT1}/etc/salt/pki/minion/
@@ -183,9 +188,8 @@ rsync -ar /etc/salt/pki/minion/ ${MNT1}/etc/salt/pki/minion/ --delete-after
 rsync -ar /root/.ssh/ ${MNT1}/root/.ssh/ --delete-after
 rsync -ar /etc/ssh/ ${MNT1}/etc/ssh/ --delete-after
 
-# if set, copy our minion ID to the new image.
-[ -e /etc/salt/minion_id ] && rsync /etc/salt/minion_id ${MNT1}/etc/salt/minion_id
 systemctl --root=${MNT1}/ enable salt-minion
+systemctl --root=${MNT1}/ enable salt-master
 
 # Set our imaging done flag(s) on both live root and imaged root.
 date --rfc-3339=seconds >${DONE_MARKER}
